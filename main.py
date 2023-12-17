200d7e1164e3566a09a7f5214e6fcc27be3c9f69
Laura <laurahara01@gmail.com>
2023-12-17 17:59:11 +0200

    googleNet with filters

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def gpuConfig():
 if __name__ == '__main__':
     data = dataSet(75)
 
-    model_name = "other"
+    model_name = "with_filters"
 
     gpuConfig()
 
@@ -57,18 +57,12 @@ if __name__ == '__main__':
         plt.legend(['test', 'train'], loc='upper left')
         plt.show()
     elif model_name == "with_filters":
-        data = dataSet(60)
         model = ModifiedGoogLeNet().getModel()
-        model.compile(optimizer='adam', loss=tf.losses.BinaryCrossentropy(from_logits=1), metrics=['accuracy'])
+        model.compile(optimizer='adam', loss=tf.losses.BinaryCrossentropy(from_logits=False), metrics=['accuracy'])
 
-        input_train = np.array([p[0] for p in data.trainingSet]).reshape(-1, 287, 200, 1)
-        output_train = np.asarray([p[1] for p in data.trainingSet])
-
-        input_test = np.array([p[0] for p in data.validationSet]).reshape(-1, 287, 200, 1)
-        output_test = np.asarray([p[1] for p in data.validationSet])
-
-        history = model.fit(input_train, output_train, batch_size=len(data.trainingSet) // 14, epochs=100,
+        history = model.fit(input_train, output_train, batch_size=len(data.trainingSet) // 11, epochs=25,
                             validation_data=(input_test, output_test))
+
         print(history.history.keys())
         # summarize history for accuracy
         plt.plot(history.history['val_loss'])
